db57b82ad2d13ce3cfce0c4783dd4991d0b6d390
Analytics-Avenue <supportteam@analyticsavenue.in>
2025-11-15 17:38:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ use_cases = {
         "image": os.path.join(ASSETS_DIR, "real_estate_preview.jpg"),
         "page": "realestate2.py"
     },
-    "Real Estate Intelligence Suite": {
+    "#1 Real Estate Intelligence Suite": {
         "image": os.path.join(ASSETS_DIR, "real_estate_preview.jpg"),
         "page": "3.py"
     }
