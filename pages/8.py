8b17c351e7082d0fd5583419d35f3ffffcdee5f0
Analytics-Avenue <supportteam@analyticsavenue.in>
2025-11-15 19:54:14 +0530

    Update 8.py

--- pages/8.py
+++ pages/8.py
@@ -115,7 +115,7 @@ with tab2:
 
     # 1. DEFAULT DATASET
     if mode == "Default Dataset":
-        URL = "https://raw.githubusercontent.com/Analytics-Avenue/streamlit-dataapp/main/datasets/RealEstate/real_estate_data.csv"
+        URL = "https://raw.githubusercontent.com/Analytics-Avenue/streamlit-dataapp/main/datasets/RealEstate/real_estate.csv"
         try:
             df = pd.read_csv(URL)
             st.success("Default dataset loaded successfully.")
