c7927bc102885c33f7255700d0065696f79de985
Analytics-Avenue <supportteam@analyticsavenue.in>
2025-11-16 15:51:35 +0530

    Update marketing_1.py

--- pages/marketing_1.py
+++ pages/marketing_1.py
@@ -93,7 +93,7 @@ with tab2:
     # DEFAULT DATASET
     # -------------------------------
     if mode == "Default Dataset":
-        URL = "https://raw.githubusercontent.com/Analytics-Avenue/streamlit-dataapp/main/datasets/Marketing_Analytics.csv"
+        URL = "https://raw.githubusercontent.com/Analytics-Avenue/streamlit-dataapp/main/datasets/marketing_analytics/marketing.csv"
 
         try:
             df = pd.read_csv(URL)
