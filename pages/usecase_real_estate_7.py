a4e5d4b240d95cf0173b0accf3b4ac5ad1743ef2
Analytics-Avenue <supportteam@analyticsavenue.in>
2025-11-18 16:40:50 +0530

    Update usecase_real_estate_7.py

--- pages/usecase_real_estate_7.py
+++ pages/usecase_real_estate_7.py
@@ -11,6 +11,22 @@ from sklearn.preprocessing import OneHotEncoder
 # ---------------------------- Page Config ----------------------------
 st.set_page_config(page_title="Rental Yield & Investment Analyzer", layout="wide")
 
+
+# -------------------------
+# Company Logo + Name
+# -------------------------
+logo_url = "https://raw.githubusercontent.com/Analytics-Avenue/streamlit-dataapp/main/logo.png"
+st.markdown(f"""
+<div style="display: flex; align-items: center;">
+    <img src="{logo_url}" width="60" style="margin-right:10px;">
+    <div style="line-height:1;">
+        <div style="color:#064b86; font-size:36px; font-weight:bold; margin:0; padding:0;">Analytics Avenue &</div>
+        <div style="color:#064b86; font-size:36px; font-weight:bold; margin:0; padding:0;">Advanced Analytics</div>
+    </div>
+</div>
+""", unsafe_allow_html=True)
+
+
 # ---------------------------- CSS ----------------------------
 hide_sidebar = """
 <style>
