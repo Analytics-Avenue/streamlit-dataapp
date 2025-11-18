b4370d6338ee3abc663f52d3b7d1c9938cb2dbab
Analytics-Avenue <supportteam@analyticsavenue.in>
2025-11-18 13:08:06 +0530

    Update marketing_5.py

--- pages/marketing_5.py
+++ pages/marketing_5.py
@@ -27,8 +27,8 @@ st.markdown(f"""
 <div style="display: flex; align-items: center;">
     <img src="{logo_url}" width="60" style="margin-right:10px;">
     <div style="line-height:1;">
-        <div style="color:#0073e6; font-size:36px; font-weight:bold; margin:0; padding:0;">Analytics Avenue &</div>
-        <div style="color:#555555; font-size:36px; font-weight:bold; margin:0; padding:0;">Advanced Analytics</div>
+        <div style="color:#064b86; font-size:36px; font-weight:bold; margin:0; padding:0;">Analytics Avenue &</div>
+        <div style="color:#064b86; font-size:36px; font-weight:bold; margin:0; padding:0;">Advanced Analytics</div>
     </div>
 </div>
 """, unsafe_allow_html=True)
