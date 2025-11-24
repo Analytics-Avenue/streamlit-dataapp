97770ed9b19cec3f1b1219922f8e8209682a267e
Analytics-Avenue <supportteam@analyticsavenue.in>
2025-11-24 12:56:01 +0530

    Update supply_chain_1.py

--- pages/supply_chain_1.py
+++ pages/supply_chain_1.py
@@ -19,6 +19,18 @@ from folium.plugins import MarkerCluster
 from streamlit_folium import st_folium
 
 warnings.filterwarnings("ignore")
+
+# -------------------------
+
+hide_sidebar = """
+<style>
+[data-testid="stSidebarNav"] {display: none;}
+section[data-testid="stSidebar"] {display: none;}
+</style>
+"""
+st.markdown(hide_sidebar, unsafe_allow_html=True)
+
+
 # -------------------------
 # Company Logo + Name
 # -------------------------
