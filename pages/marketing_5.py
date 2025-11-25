10951564aaca433c163b8a587ababc3c12fa8786
Analytics-Avenue <supportteam@analyticsavenue.in>
2025-11-25 16:26:53 +0530

    Update marketing_5.py

--- pages/marketing_5.py
+++ pages/marketing_5.py
@@ -14,6 +14,17 @@ import math
 import warnings
 warnings.filterwarnings("ignore")
 
+
+# -------------------------
+
+hide_sidebar = """
+<style>
+[data-testid="stSidebarNav"] {display: none;}
+section[data-testid="stSidebar"] {display: none;}
+</style>
+"""
+st.markdown(hide_sidebar, unsafe_allow_html=True)
+
 # -------------------------
 # Page / App Config
 # -------------------------
